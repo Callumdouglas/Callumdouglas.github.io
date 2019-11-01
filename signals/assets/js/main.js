9edf52322f362413a5023f1742fed60bd80732c6
Callumdouglas <54167707+Callumdouglas@users.noreply.github.com>
2019-11-01 15:41:14 +1100

    reset

---
+++ signals/assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	var hideAll = function() {
+		$('.research-item').addClass('hide');
+	}
+
+		$('.btn-gym').click(function(){
+			hideAll();
+		$('.gym').removeClass('hide');
+	});
+
+	var hideAll = function() {
+		$('.research-item').addClass('hide');
+	}
+
+		$('.btn-neo').click(function(){
+			hideAll();
+		$('.neo').removeClass('hide');
+	});
+
+		var hideAll = function() {
+		$('.research-item').addClass('hide');
+	}
+
+		$('.btn-e').click(function(){
+			hideAll();
+		$('.e').removeClass('hide');
+	});
+
+	
+});
